bb4b3d0bb353d0401a20e9ce79ae7ddf6f13e1ef
fatimazaarate <fatima.zaarate@gmail.com>
2023-08-02 10:22:06 -0700

    print str in rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-/*
- *  _print_rev_recursion - prints a string in reverse
- *  @s : input
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: string to be printed.
  *  return : always 0
  */
 
