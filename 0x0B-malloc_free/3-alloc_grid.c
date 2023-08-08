baf6bb1fc4ed9380487b3655dbf04a0f008786d7
fatimazaarate <fatima.zaarate@gmail.com>
2023-08-08 21:19:47 +0100

    alloc grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * height: height input
+ *
+ * Return: pointer to 2 dimensions array
+ **/
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i, j;
+
+	ptr = malloc(sizeof(int *) * height);
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+
+		if (ptr[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(ptr[i]);
+			free(ptr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	}
+	return (ptr);
+}
+
