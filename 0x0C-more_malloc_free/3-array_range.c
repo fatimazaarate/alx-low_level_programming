979f37b4b3cf04a06a3392fdc6ea1058eb1ab64a
fatimazaarate <fatima.zaarate@gmail.com>
2023-08-10 20:36:25 +0100

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -23,7 +23,8 @@ int *array_range(int min, int max)
 
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; min <= max; i++)
-		ptr[i] = min++;
+	for (i = 0; i < size; i++)
+		ptr[i] = min + i;
 
 	return (ptr);
+}
