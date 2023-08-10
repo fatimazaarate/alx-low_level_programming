f830e76b90a482f62219073837f946eac6393430
fatimazaarate <fatima.zaarate@gmail.com>
2023-08-10 17:02:34 +0100

    string n concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,16 +14,16 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, j, N = n, size;
+	int i, j, size;
+	int N = n;
 	int k = 0;
-	int len1, len2;
+	int len1 = 0, len2 = 0;
 	char *concat;
 
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s1 == NULL)
 		s2 = "";
-	}
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
