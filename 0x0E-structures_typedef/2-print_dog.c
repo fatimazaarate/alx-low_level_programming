dfa9f55591c2b6044379e03ea82b068198777edf
fatimazaarate <fatima.zaarate@gmail.com>
2023-08-14 17:29:41 +0100

    print

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL || d->age == 0 || d->owner == NULL)
+		printf("nil");
+
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
