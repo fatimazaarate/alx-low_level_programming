49b73160e46762e183761fbc73061f7d5b074a3a
fatimazaarate <fatima.zaarate@gmail.com>
2023-08-16 22:12:00 +0100

    calc

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,15 @@
+#include "calc.h"
+
+/**
+ *
+ */
+int (*get_op_func(char *s))(int, int)
+{
+#include "calc.h"
+
+	/**
+	 *
+	 */
+	int (*get_op_func(char *s))(int, int)
+	{
+
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,9 +1,51 @@
 #include "calc.h"
 
+/**
+ * op_add - adds tow numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: Always success(0)
+ */
 int op_add(int a, int b)
 	return (a + b);
 
+/**
+ * op_sub - substitution of tow numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: Always success(0)
+ */
 int op_sub(int a, int b)
 	return (a - b);
 
+/**
+ * op_mul - multiplies tow numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: Always success(0)
+ */
 int op_mul(int a, int b)
+	return (a * b);
+
+/**
+ * op_div - divides tow numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: Always success(0)
+ */
+int op_div(int a, int b)
+	return (a / b);
+
+/**
+ * op_mod - the remainder of the division
+ * @a: first number
+ * @b: second number
+ *
+ * Return: Always success(0)
+ */
+int op_mod(int a, int b)
+	return (a % b);
