0cd312edab805f549a747fc31ca888ed48e75fd2
fatimazaarate <fatima.zaarate@gmail.com>
2023-08-24 23:06:43 +0100

    List length

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,13 +11,13 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while(h)
+	while (h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 
 		else
-			printf("[%u] %s\n",h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
 		count++;
