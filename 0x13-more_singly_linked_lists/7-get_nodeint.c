0b7c3c5084a970dcc86ac3a01388e32dbb8270a2
fatimazaarate <fatima.zaarate@gmail.com>
2023-08-29 18:36:34 +0100

    Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		if (count == index)
 			return (head);
 
-		head = had->next;
+		head = head->next;
 		count++;
 	}
 	return (NULL);
