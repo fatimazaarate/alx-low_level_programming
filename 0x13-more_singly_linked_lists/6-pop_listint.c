68d02b1416ef1bbabcedf8a30cdc5d64e22c1e34
fatimazaarate <fatima.zaarate@gmail.com>
2023-08-29 16:22:53 +0100

    6. Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+* pop_listint - for being rude
+* @head: the master of them
+* Return: value of delted node
+*/
+
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
@@ -8,11 +14,11 @@ int pop_listint(listint_t **head)
 	if (!(*head))
 		return (0);
 
+	nb = (*head)->n;
 	tmp = (*head)->next;
 
 	free(*head);
 
 	*head = tmp;
-	nb = (*head)->n;
 	return (nb);
 }
