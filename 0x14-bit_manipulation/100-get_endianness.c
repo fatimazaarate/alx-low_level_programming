8a5ab951aeb857067f4861658974fb311ea042aa
fatimazaarate <fatima.zaarate@gmail.com>
2023-09-06 18:45:07 +0100

    endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,11 +7,11 @@
 
 int get_endianness(void)
 {
-	unsigned int i = 1;
-	char *c = (unsigned char *) &i;
+	int i = 1;
+	char *c = (char *) &i;
 
 	if (*c == 1)
 		return (1);
-	else
-		return (0);
+
+	return (0);
 }
