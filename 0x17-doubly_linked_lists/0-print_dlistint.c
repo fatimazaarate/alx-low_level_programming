9230f708ec036a13ddbea2e7f5dd23201af67e2d
fatimazaarate <fatima.zaarate@gmail.com>
2023-10-12 15:58:02 +0100

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,15 +9,15 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *tmp;
-    size_t count = 0;
+	const dlistint_t *tmp;
+	size_t count = 0;
 
-    tmp = h;
-    while(tmp)
-    {
-        printf("%d\n", tmp->n);
-        tmp = tmp->next;
-        count++;
-    }
-    return (count);
+	tmp = h;
+	while(tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		count++;
+	}
+	return (count);
 }
