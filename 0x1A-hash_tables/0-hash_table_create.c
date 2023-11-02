6cf581884e50c2270a9ed157a9b5e4721aabc220
fatimazaarate <fatima.zaarate@gmail.com>
2023-11-02 20:40:52 +0100

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,6 +3,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *new_table = NULL;
+    unsigned long int i;
 
     new_table = (hash_table_t*) malloc(sizeof(hash_table_t));
     if (!new_table)
@@ -10,6 +11,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
     new_table->size = size;
     new_table->array = (hash_node_t**) calloc(new_table->size, sizeof(hash_node_t));
+
+    for (i = 0; i < new_table->size; i++)
+        new_table->array[i] = NULL;
 
     return new_table;
 }
