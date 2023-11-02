50277e50d910822a7b99ec1e65b3525cbe23419f
fatimazaarate <fatima.zaarate@gmail.com>
2023-11-02 20:43:46 +0100

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *new_table = NULL;
-    unsigned long int i;
 
     new_table = (hash_table_t*) malloc(sizeof(hash_table_t));
     if (!new_table)
